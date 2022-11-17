32b1e911d146ba28a4fbf456889e3802f1597c8b
Jeff Napper (jenapper) <jenapper@cisco.com>
2022-11-17 09:11:28 +0100

    Added action workflow to build Docker container.

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,12 @@ COPY generate_translations.js \
 
 COPY lemmy-translations lemmy-translations
 COPY src src
-COPY .git .git
 
-# Set UI version 
-RUN echo "export const VERSION = '$(git describe --tag)';" > "src/shared/version.ts"
+ARG VERSION="unknown"
+ARG COMMIT="unknown"
+
+# Set UI version
+RUN echo "export const VERSION = '${VERSION}';\nexport const COMMIT = '${COMMIT}';" > "src/shared/version.ts"
 
 RUN yarn
 RUN yarn build:prod
@@ -27,6 +29,15 @@ RUN yarn build:prod
 FROM node:alpine as runner
 COPY --from=builder /usr/src/app/dist /app/dist
 COPY --from=builder /usr/src/app/node_modules /app/node_modules
+
+ENV LEMMY_UI_HOST="0.0.0.0:1234"
+ENV LEMMY_UI_EXTRA_THEMES_FOLDER="./extra_themes"
+ENV LEMMY_UI_DEBUG="false"
+
+ENV LEMMY_INTERNAL_HOST="0.0.0.0:8536"
+ENV LEMMY_EXTERNAL_HOST="0.0.0.0:8536"
+ENV LEMMY_WS_HOST="0.0.0.0:8536"
+ENV LEMMY_HTTPS="false"
 
 EXPOSE 1234
 WORKDIR /app
--- src/shared/components/app/footer.tsx
+++ src/shared/components/app/footer.tsx
@@ -19,14 +19,6 @@ export class Footer extends Component<FooterProps, any> {
       <nav class="container navbar navbar-expand-md navbar-light navbar-bg p-3">
         <div className="navbar-collapse">
           <ul class="navbar-nav ml-auto">
-            {this.props.site.version !== VERSION && (
-              <li class="nav-item">
-                <span class="nav-link">UI: {VERSION}</span>
-              </li>
-            )}
-            <li class="nav-item">
-              <span class="nav-link">BE: {this.props.site.version}</span>
-            </li>
             <li className="nav-item">
               <NavLink className="nav-link" to="/modlog">
                 {i18n.t("modlog")}
@@ -39,28 +31,6 @@ export class Footer extends Component<FooterProps, any> {
                 </NavLink>
               </li>
             )}
-            {this.props.site.federated_instances && (
-              <li class="nav-item">
-                <NavLink className="nav-link" to="/instances">
-                  {i18n.t("instances")}
-                </NavLink>
-              </li>
-            )}
-            <li class="nav-item">
-              <a className="nav-link" href={docsUrl}>
-                {i18n.t("docs")}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href={repoUrl}>
-                {i18n.t("code")}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href={joinLemmyUrl}>
-                {i18n.t("join_lemmy")}
-              </a>
-            </li>
           </ul>
         </div>
       </nav>
